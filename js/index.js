f96bef58471930d420cf777ccde6bcb3070dfc5c
Javier <tsakasu88@gmail.com>
2019-10-28 19:00:12 -0400

    added javascript for html

--- js/index.js
+++ js/index.js
@@ -38,5 +38,92 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// // Selector Elements 
+
+const tagElements = document.getElementsByTagName('a');
+console.log(tagElements[1]);
+
+const navSelection = document.querySelector('nav');
+
+// Img Src updates 
+
+const logoImg = document.querySelector('#logo-img');
+logoImg.src = './img/logo.png';
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = './img/header-img.png';
+
+const midImg = document.querySelector ('#middle-img');
+midImg.src = './img/mid-page-accent.jpg';
+
+// Update Html with The JavaScript information given
+
+// Navigation
+
+const navItems = document.querySelectorAll('a');
+navItems[0].textContent = "Services";
+navItems[1].textContent = "Product";
+navItems[2].textContent = "Vision";
+navItems[3].textContent = "Features";
+navItems[4].textContent = "About";
+navItems[5].textContent = "Contact";
+
+// Nav Colors
+
+navItems[0].style.color = "green";
+navItems[1].style.color = "green";
+navItems[2].style.color = "green";
+navItems[3].style.color = "green";
+navItems[4].style.color = "green";
+navItems[5].style.color = "green";
+
+// CTA section
+
+const mainTitle = document.querySelector('h1'); 
+mainTitle.textContent = "DOM is AWESOME";
+
+const buttonText = document.querySelector('button');
+buttonText.textContent = "Get Started";
+
+// Content for all the Header 4 
+
+const infoForH4 = document.querySelectorAll('h4');
+infoForH4[0].textContent = "Features";
+infoForH4[1].textContent = "About";
+infoForH4[2].textContent = "Services";
+infoForH4[3].textContent = "Product";
+infoForH4[4].textContent = "Vision";
+infoForH4[5].textContent = "Contact";
+
+// Info for Paragraphs 
+
+const pInfo = document.querySelectorAll ('p');
+pInfo[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+pInfo[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+pInfo[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+pInfo[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+pInfo[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+pInfo[5].textContent = '123 Way 456 Street Somewhere, USA';
+pInfo[6].textContent = '1 (888) 888-8888';
+pInfo[7].textContent = 'sales@greatidea.io';
+pInfo[8].textContent = 'Copyright Great Idea! 2019';
+
+
+// New Content 
+
+const newNavOne = document.createElement('a');
+const newNavTwo = document.createElement('a');
+
+const mainNav = document.querySelector('nav');
+
+newNavOne.innerText = 'Sign up';
+newNavTwo.innerText = 'Staff';
+
+mainNav.append(newNavOne);
+mainNav.prepend(newNavTwo);
+
+
+
